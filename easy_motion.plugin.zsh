3b8bc748583d979a845e3fb04539194eb27e04aa
Ingo Heimbach <IJ_H@gmx.de>
2017-11-19 21:09:12 +0100

    Increment the cursor position in operator pending mode if needed
    In operator pending mode (change or delete) some motions must return an
    incremented cursor position. Example: if you delete `word` till the end
    the character `d` would remain without this patch. In this case, the
    cursor is incremented by one to delete `d` as well.

--- easy_motion.plugin.zsh
+++ easy_motion.plugin.zsh
@@ -1,9 +1,14 @@
 _EASY_MOTION_ROOT_DIR="${0:h}"
+# in a script, we cannot query if zsh is in vi operator pending mode
+# -> keep track of it
+_EASY_MOTION_VIOPP=0
+_EASY_MOTION_TIMESTAMP_CMD="python -c 'import time; print(int(time.time() * 1000))'"
 
 function _easy-motion () {
     local -a MOTIONS_WITH_ARGUMENT=( "f" "F" "t" "T" "s" )
     local -a TARGET_KEYS=( "a" "s" "d" "g" "h" "k" "l" "q" "w" "e" "r" "t" "y" "u" \
         "i" "o" "p" "z" "x" "c" "v" "b" "n" "m" "f" "j" ";" )
+    local MAX_VIOPP_TIME_DELTA=500
     local motion
     local motion_argument
     local -a motion_indices
@@ -15,10 +20,16 @@ function _easy-motion () {
     local ret
 
     function _easy-motion-save-state {
+        local current_time
+        current_time="$(python -c 'import time; print(int(time.time() * 1000))')"
         saved_buffer="${BUFFER}"
         saved_predisplay="${PREDISPLAY}"
         saved_postdisplay="${POSTDISPLAY}"
         saved_region_highlight=("${region_highlight[@]}")
+        # Assume that operator pending mode was already exited if ${MAX_VIOPP_TIME_DELTA} milliseconds are over
+        if (( ${_EASY_MOTION_VIOPP} )) && (( ${current_time} - ${_EASY_MOTION_VIOPP_TIMESTAMP} > ${MAX_VIOPP_TIME_DELTA} )); then
+            _EASY_MOTION_VIOPP=0
+        fi
     }
 
     function _easy-motion-read-motion {
@@ -76,6 +87,13 @@ function _easy-motion () {
         (( ${target_index} <= ${#motion_indices})) || return 7
         # Move the cursor to the chosen target
         CURSOR="${motion_indices[${target_index}]}"
+        if (( ${_EASY_MOTION_VIOPP} )); then
+            case $motion in
+                e|E|ge|gE|f|t|s)
+                    (( CURSOR++ ))
+                    ;;
+            esac
+        fi
         zle -R
         return 0
     }
@@ -85,6 +103,7 @@ function _easy-motion () {
         PREDISPLAY="${saved_predisplay}"
         POSTDISPLAY="${saved_postdisplay}"
         region_highlight=("${saved_region_highlight[@]}")
+        _EASY_MOTION_VIOPP=0
     }
 
     _easy-motion-save-state && \
@@ -97,5 +116,20 @@ function _easy-motion () {
 
     return "${ret}"
 }
+
+function vi-change-wrapper () {
+    _EASY_MOTION_VIOPP=1
+    eval _EASY_MOTION_VIOPP_TIMESTAMP="$(python -c 'import time; print(int(time.time() * 1000))')"
+    zle vi-change
+}
+function vi-delete-wrapper () {
+    _EASY_MOTION_VIOPP=1
+    eval _EASY_MOTION_VIOPP_TIMESTAMP="$(python -c 'import time; print(int(time.time() * 1000))')"
+    zle vi-delete
+}
+zle -N vi-change-wrapper
+zle -N vi-delete-wrapper
+bindkey -M vicmd c vi-change-wrapper
+bindkey -M vicmd d vi-delete-wrapper
 
 zle -N _easy-motion
