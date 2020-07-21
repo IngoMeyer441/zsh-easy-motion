29867d84443af180ceddb98c098cb0312c5b80e5
Ingo Heimbach <IJ_H@gmx.de>
2020-07-21 11:51:21 +0200

    Rework `motion2indices.py` to support index groups

--- motion2indices.py
+++ motion2indices.py
@@ -8,11 +8,20 @@ from __future__ import unicode_literals
 
 import re
 import sys
-from itertools import zip_longest
+
+PY2 = sys.version_info.major < 3  # is needed for correct mypy checking
+
+from itertools import chain
+
+if PY2:
+    from itertools import izip_longest as zip_longest
+else:
+    from itertools import zip_longest
 
 try:
     from typing import (  # noqa: F401  # pylint: disable=unused-import
         cast,
+        Any,
         AnyStr,
         Callable,
         Dict,
@@ -27,8 +36,6 @@ try:
 except ImportError:
     cast = lambda t, x: x  # type: ignore  # noqa: E731
     Text = unicode if (sys.version_info.major < 3) else str  # type: ignore
-
-PY2 = sys.version_info.major < 3  # is needed for correct mypy checking
 
 VALID_MOTIONS = frozenset(("b", "B", "ge", "gE", "e", "E", "w", "W", "f", "F", "t", "T", "s", "c"))
 MOTIONS_WITH_ARGUMENT = frozenset(("f", "F", "t", "T", "s"))
@@ -60,6 +67,14 @@ class InvalidCursorPositionError(Exception):
     pass
 
 
+class MissingGroupLengthError(Exception):
+    pass
+
+
+class InvalidGroupLengthError(Exception):
+    pass
+
+
 class InvalidMotionError(Exception):
     pass
 
@@ -77,7 +92,7 @@ class MissingTextError(Exception):
 
 
 def parse_arguments():
-    # type: () -> Tuple[int, Text, Text, Optional[Text]]
+    # type: () -> Tuple[int, int, Text, Text, Optional[Text]]
     if PY2:
         argv = [arg.decode("utf-8") for arg in sys.argv]
     else:
@@ -88,8 +103,14 @@ def parse_arguments():
     if not argv:
         raise MissingCursorPositionError("No cursor position given.")
     if not argv[0].isdigit():
-        raise InvalidCursorPositionError('The cursor "{}" position is not a number.'.format(argv[0]))
+        raise InvalidCursorPositionError('The cursor position "{}" is not a number.'.format(argv[0]))
     cursor_position = int(argv.pop(0))
+    # Extract group length
+    if not argv:
+        raise MissingGroupLengthError("No group length given.")
+    if not argv[0].isdigit():
+        raise InvalidGroupLengthError('The group length "{}" is not a number.'.format(argv[0]))
+    group_length = int(argv.pop(0))
     # Extract motion
     if not argv:
         raise MissingMotionError("No motion given.")
@@ -107,7 +128,7 @@ def parse_arguments():
     if not argv:
         raise MissingTextError("No text given.")
     text = " ".join(argv)
-    return cursor_position, text, motion, motion_argument
+    return cursor_position, group_length, text, motion, motion_argument
 
 
 def motion_to_indices(cursor_position, text, motion, motion_argument):
@@ -147,15 +168,71 @@ def motion_to_indices(cursor_position, text, motion, motion_argument):
     return indices
 
 
+def group_indices(indices, group_length):
+    # type: (Iterable[int], int) -> Iterable[Any]
+
+    def group(indices, group_length):
+        # type: (Iterable[int], int) -> Union[Iterable[Any], int]
+        def find_required_slot_sizes(num_indices, group_length):
+            # type: (int, int) -> List[int]
+            if num_indices <= group_length:
+                slot_sizes = num_indices * [1]
+            else:
+                slot_sizes = group_length * [1]
+                next_increase_slot = group_length - 1
+                while sum(slot_sizes) < num_indices:
+                    slot_sizes[next_increase_slot] *= group_length
+                    next_increase_slot = (next_increase_slot - 1 + group_length) % group_length
+                previous_increase_slot = (next_increase_slot + 1) % group_length
+                # Always fill rear slots first
+                slot_sizes[previous_increase_slot] -= sum(slot_sizes) - num_indices
+            return slot_sizes
+
+        indices_as_tuple = tuple(indices)
+        num_indices = len(indices_as_tuple)
+        if num_indices == 1:
+            return indices_as_tuple[0]
+        slot_sizes = find_required_slot_sizes(num_indices, group_length)
+        slot_start_indices = [0]
+        for slot_size in slot_sizes[:-1]:
+            slot_start_indices.append(slot_start_indices[-1] + slot_size)
+        grouped_indices = (
+            group(indices_as_tuple[slot_start_index : slot_start_index + slot_size], group_length)
+            for slot_start_index, slot_size in zip(slot_start_indices, slot_sizes)
+        )
+        return grouped_indices
+
+    grouped_indices = group(indices, group_length)
+    if isinstance(grouped_indices, int):
+        return (grouped_indices,)
+    else:
+        return grouped_indices
+
+
+def print_grouped_indices(grouped_indices, recursion_depth=0):
+    # type: (Iterable[Any], int) -> None
+    print(recursion_depth * ">", end="")
+    for group_or_index in grouped_indices:
+        if isinstance(group_or_index, int):
+            print("{:d} ".format(group_or_index), end="")
+        else:
+            print()
+            print_grouped_indices(group_or_index, recursion_depth + 1)
+
+
 def main():
     # type: () -> None
     try:
-        cursor_position, text, motion, motion_argument = parse_arguments()
+        cursor_position, group_length, text, motion, motion_argument = parse_arguments()
         indices = motion_to_indices(cursor_position, text, motion, motion_argument)
+        grouped_indices = group_indices(indices, group_length)
+        print_grouped_indices(grouped_indices)
         print(" ".join(Text(index) for index in indices))
     except (
         MissingCursorPositionError,
         InvalidCursorPositionError,
+        MissingGroupLengthError,
+        InvalidGroupLengthError,
         InvalidMotionError,
         MissingMotionError,
         MissingMotionArgumentError,
